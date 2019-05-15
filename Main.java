70d478e77b041cb15218c4d0ccd4c99892feb59e
Adèle Richard <richaradele@gmail.com>
2019-05-15 13:50:58 +0200

    toMerge

--- Main.java
+++ Main.java
@@ -1,15 +1,53 @@
+import src.utilities.*;
 import java.util.ArrayList;
 import java.util.HashMap;
-import src.utilities.*;
+import java.util.Scanner;
+//import src.utilities.*;
 public class Main {
     public static void main(String[] args) throws Exception {
-        String filePath = System.getProperty("user.dir");
-        try {
-            ReadFile rf = new ReadFile();
-            HashMap<String , ArrayList<Double>> wordsVectors = rf.readVectors(filePath);
-            System.out.println(wordsVectors.get("mère"));
-        } catch (Exception e) {
-            System.out.println("Votre fichier w2v_final3 doit être à la racine du projet");
+        String filePath = "";
+        int nbPlayers = 0;
+        int nbTry = 0;
+        int nbRes = 0;
+        HashMap<String , ArrayList<Double>> wordsVectors;
+        
+        //java Main --w2v ../Geenson_Game/ --nbPlayers 2 --nbTry 3 --k 3 --magicalDice
+        
+        for(int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "--w2v" :
+                    filePath = args[1];
+                    break;
+                case "--nbPlayers" :
+                    nbPlayers = Integer.parseInt(args[3]);
+                    break;
+                case "--nbTry" :
+                    nbTry = Integer.parseInt(args[5]);
+                    break;
+                case "--k" :
+                    nbRes = Integer.parseInt(args[7]);
+                    break;
+                case "--magicalDice" :
+                    //Dice md = new Dice();
+                    break;
+            }
         }
+        
+        GameOn game = new GameOn(nbPlayers, nbTry, nbRes);
+
+        System.out.println("var = " + filePath + " " + nbPlayers + " " + nbTry + " " + nbRes);
+
+        /* try {
+            
+            //System.out.println(wordsVectors.get("ordinateur"));
+        } catch (Exception e) {
+            System.out.println("Votre fichier w2v_final3 n'est pas dans le répertoir spécifié");
+        } */
+
+        ReadFile rf = new ReadFile();
+        wordsVectors = rf.readVectors(filePath);
+        String wordToGuess = game.getRandWord(wordsVectors);
+        game.showWords(wordToGuess);        
+        
     }
 }
--- src/utilities/UtilVector.java
+++ src/utilities/UtilVector.java
@@ -1,4 +1,102 @@
 package src.utilities;
+import java.lang.Math.*;
 
-public class UtilVector {
+protected static class UtilVector {
+	// Méthodes
+	
+	//Renvoit le vecteur résultant de l'addition de v1 et v2
+	protected static double[] addVec(double[] v1, double[] v2) {
+		if(!sameLength(v1, v2)) {
+			return null;
+		}
+		double[] result = new double[v1.length];
+		for (int i=0; i<v1.length; i++) {
+			result[i] = v1[i]+v2[i];
+		}
+		return result;
+	}
+	
+	//Renvoit le vecteur résultant de la soustraction de v1 et v2
+	protected static double[] subVec(double[] v1, double[] v2) {
+		if(!sameLength(v1, v2)) {
+			return null;
+		}
+		double[] result = new double[v1.length];
+		for (int i=0; i<v1.length; i++) {
+			result[i] = v1[i]-v2[i];
+		}
+		return result;
+	}
+	
+	//Renvoit le vecteur résultant du produit scalaire de v1 avec scal
+	protected static double[] scalMultVec(double[] v1, double scal) {
+		double[] result = new double[v1.length];
+		for (int i=0; i<v1.length; i++) {
+			result[i] = v1[i]*scal;
+		}
+		return result;
+	}
+	
+	//Renvoit la moyenne du vecteur v1
+	protected static double meanVec(double[] v1) {
+		double result = 0;
+		double sum = 0;
+		for (int i=0; i<v1.length; i++) {
+			sum = sum + v1[i];
+		}
+		result = sum / v1.length;
+		return result;
+	}
+	
+	//Renvoit la norme du vecteur v1
+	protected static double normVec(double[] v1) {
+		double result = 0;
+		double inter = 0;
+		for (int i=0; i<v1.length; i++) {
+			inter = v1[i]*v1[i];
+		}
+		result = Math.sqrt(inter);
+		return result;
+	}	
+	
+	//Renvoit la distance euclidienne entre v1 et v2
+	protected static double distVec(double[] v1, double[] v2) {
+		if(!sameLength(v1, v2)) {
+			return 0;
+		}
+		double sum = 0;
+		for (int i=0; i<v1.length; i++) {
+			sum = sum + (v1[i] - v2[i])*(v1[i] - v2[i]);
+		}
+		double result = Math.sqrt(sum);
+		return result;
+	}
+	
+	//renvoit la similarité cosinus entre v1 et v2
+	protected static double cosVec(double[] v1, double[] v2) {
+		if(!sameLength(v1, v2)) {
+			return 0;
+		}
+		double result = 0;
+		double inter1 = 0;
+		double inter2 = 0;
+		double inter3 = 0;
+		for (int i=0; i<v1.length; i++) {
+			inter1 = inter1 + v1[i]*v2[i];
+			inter2 = inter2 + v1[i]*v1[i];
+			inter3 = inter3 + v2[i]*v2[i];
+		}
+		result = inter1 / (Math.sqrt(inter1) * Math.sqrt(inter2));
+		
+		return result;
+	}
+
+	//Si la longueur de V1 != longueur de V2
+	protected static boolean sameLength(double [] v1, double[] v2) {
+		if(v1.length != v2.length) {
+			System.out.println("Erreur, tailles incompatibles");
+			return false;
+		}
+		return true;
+	}
 }
